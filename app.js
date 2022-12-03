a4bc70f7ba0142b6741bd9453e153ddd6effe4d6
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-12-03 16:49:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,56 @@
 //jshint esversion:6
 
-
-//jshint esversion:6
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 
 const app = express();
 
-let items = ["Buy Food","Cook Food","Eat Food"];
+const items = ["Buy Food","Cook Food","Eat Food"];
+const workItems = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
 
-  let today = new Date();
+const day = date.getDate();
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
-  res.render("list", {kindOfDay: day, newListItems: items});
+  res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
-  items.push(item);
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 
-  res.redirect("/");
 });
 
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function (requ, res){
+  res.render("about");
+})
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
